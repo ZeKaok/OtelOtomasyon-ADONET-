cc8acff814ea1634e7618ea60019aa58d866212f
zehrahome <zehrakarakaya999@gmail.com>
2023-06-11 01:02:26 +0300

    Facade kısmı tamamlandı

--- ORM/Facade/Satislar.cs
+++ ORM/Facade/Satislar.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ORM.Facade
 {
-    internal class Satislar
+    internal class Satislar : IORM<Satislar>
     {
+        bool IORM<Satislar>.Delete(Satislar entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IORM<Satislar>.Insert(Satislar entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataTable IORM<Satislar>.Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IORM<Satislar>.Update(Satislar entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ORM/Facade/Tedarikciler.cs
+++ ORM/Facade/Tedarikciler.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using ORM.Entity;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ORM.Facade
 {
-    internal class Tedarikciler
+    internal class Tedarikciler : IORM<Tedarikci>
     {
+        bool IORM<Tedarikci>.Delete(Tedarikci entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IORM<Tedarikci>.Insert(Tedarikci entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataTable IORM<Tedarikci>.Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IORM<Tedarikci>.Update(Tedarikci entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
